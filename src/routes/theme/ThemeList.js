512b04f7527b857ae280f6452c8cb73c5acc1f0b
fhmanman <fhmanman@126.com>
2018-03-30 18:00:02 +0800

    'init'

---
+++ src/routes/theme/ThemeList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'dva';
+import { styles } from './index.less';
+
+class ThemeList extends React.Component {
+	state = {
+		
+	}
+	componentDidMount(){
+		
+	}
+}
+export default connect((theme)=>{{theme}})
